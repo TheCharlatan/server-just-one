82de02f5ed40c00aa238628d95027d2f733068f0
TheCharlatan <seb.kung@gmail.com>
2020-03-25 03:29:52 +0100

    Add REST api and DTO for lobby, game and overview
    This commit creates the lobby, game and overview controllers and puts in
    place the data types and classes required to implement the the /lobby
    /game and /chat endpoints as described by the spec.
    
    The tests added so far do not check content and just check for the
    correct status codes.

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/rest/dto/LobbyPostDTO.java
@@ -0,0 +1,16 @@
+package ch.uzh.ifi.seal.soprafs20.rest.dto;
+
+import java.util.ArrayList;
+
+public class LobbyPostDTO {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
