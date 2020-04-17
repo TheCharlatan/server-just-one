ae922d696741089b2c5e8b618d5c5cf1cb235faa
Unknown <stefanie.javet@hispeed.ch>
2020-04-17 16:23:41 +0200

    handle guess

--- src/main/java/ch/uzh/ifi/seal/soprafs20/service/GameService.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/service/GameService.java
@@ -7,6 +7,7 @@ import ch.uzh.ifi.seal.soprafs20.exceptions.NotFoundException;
 import ch.uzh.ifi.seal.soprafs20.exceptions.ServiceException;
 import ch.uzh.ifi.seal.soprafs20.repository.GameRepository;
 import ch.uzh.ifi.seal.soprafs20.repository.UserRepository;
+import ch.uzh.ifi.seal.soprafs20.rest.dto.GamePutDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -125,7 +126,6 @@ public class GameService {
         return words;
     }
 
-
     public void chooseWord(long gameId, int wordIndex){
         Game game = getExistingGame(gameId);
 
@@ -173,4 +173,35 @@ public class GameService {
         }
 
     }
+
+    // checks if the mysteryWord matches with the guess
+    public GamePutDTO checkGuess(GamePutDTO gamePutDTO, long id) {
+        int index = gamePutDTO.getWordIndex();
+        String guess = gamePutDTO.getGuess();
+        Game game = this.gameRepository.findById(id).get();
+        String mysteryWord = game.getWords().get(index);
+
+
+        if (mysteryWord.equals(guess)) {
+            gamePutDTO.setGuessCorrect(true);
+        }
+        else {
+            gamePutDTO.setGuessCorrect(false);
+        }
+
+        return gamePutDTO;
+    }
+
+    private void roundEnd (Game game) {
+        //update the score of the active player
+        //if game.round + 1 is less or equal 13 a new round is started
+            //choose a new active player
+            // provide new list of words
+            //update game status
+            //reset list of clues
+        //else
+            //set game status = "GAME OVER"
+            //
+    }
+
 }
